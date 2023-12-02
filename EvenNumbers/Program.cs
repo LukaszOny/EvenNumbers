7874d6856d7154d320f09d136cf2f33e0fab81b0
lonys <lonys@LAPTOP-8GOJV76L>
2023-12-02 17:57:12 +0100

    Add project files.

---
+++ EvenNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("EVEN NUMBERS FROM 0 TO 1000");
+Console.WriteLine("<<<<<<<<<<<<<<>>>>>>>>>>>>>>");
+int a = 0;
+do
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine($"The Even number: {a}");
+    }
+    a++;
+}
+while (a<=1000);
+
+
+
